c168f23c5be38b5bfdba4d327d49f19668422c12
Brian Sak <briansak@users.noreply.github.com>
2023-12-04 17:52:28 -0600

    Update write-l3-policy.py

--- write-l3-policy.py
+++ write-l3-policy.py
@@ -4,11 +4,21 @@ import json
 API_KEY = 'API_KEY'
 dashboard = meraki.DashboardAPI(API_KEY, suppress_logging=True)
 
-network_id = 'NETWORK_ID'
+pod_number = 'YOUR_POD_NUMBER'
+network_id = 'YOUR_NETWORK_ID'
+org_id = 'YOUR_ORG_ID'
 
-response = dashboard.appliance.updateNetworkApplianceFirewallInboundFirewallRules(
-    network_id, 
-    rules=[{'comment': 'Allow Cisco Access to Servers', 'policy': 'allow', 'protocol': 'tcp', 'destPort': '443', 'destCidr': 'Any', 'srcPort': 'Any', 'srcCidr': '2001:420::/32', 'syslogEnabled': False}]
-)
+new_acl = []
+acl = dashboard.appliance.getNetworkApplianceFirewallInboundFirewallRules('network_id')
+for rule in acl["rules"]:
+  if rule["comment"] != "Default rule":
+    new_acl.append(rule)
 
-print(json.dumps(response, indent=4))
+new_acl.append({'comment': f'Allow Pod {pod_number} Access to Servers', 'policy': 'allow', 'protocol': 'tcp', 'destPort': '443', 'destCidr': 'Any', 'srcPort': 'Any', 'srcCidr': f'2003:{pod_number}::/32', 'syslogEnabled': False})
+
+networks = dashboard.organizations.getOrganizationNetworks(org_id, total_pages='all')
+for network in networks:
+  response = dashboard.appliance.updateNetworkApplianceFirewallInboundFirewallRules(network['id'], rules = new_acl)
+  print(f'Wrote firewall ACE to {network["name"]}')
+
+print('All Done!')
